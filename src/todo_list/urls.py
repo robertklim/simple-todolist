2482be3bc7323ecb65277d409e52904be66372cb
Robert Klim <kontakt.robert.klim@gmail.com>
2018-07-18 17:52:23 +0200

    Add edit

--- src/todo_list/urls.py
+++ src/todo_list/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('delete/<list_id>', views.delete, name="delete"),
     path('cross/<list_id>', views.cross, name="cross"),
     path('uncross/<list_id>', views.uncross, name="uncross"),
+    path('edit/<list_id>', views.edit, name="edit"),
 ]
